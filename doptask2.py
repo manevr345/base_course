7145cbcd5ea83a9584268dcb769bc04c6875c3f5
manevr345 <manevrik39@yandex.ru>
2025-10-16 15:53:57 +0000

    sosi

---
+++ doptask2.py
@@ -0,0 +1,5 @@
+num1 = int(input())
+num2 = num1+1
+num3 = num1-1
+print(f'Следующее число за {num1}: {num2}')
+print(f'Предыдущее число за {num1}: {num3}')
---
+++ doptask4.py
@@ -0,0 +1,4 @@
+num1 = int(input('Введите Число 1: '))
+num2 = int(input("Введите Число 2: "))
+value = ((num1 + num2) ** 3)*3 + 275*(num2 ** 2) - 127 * num1 - 41
+print(value)
