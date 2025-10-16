c6e17470df5ae73a48281b25751f7767ddd64741
manevr345 <manevrik39@yandex.ru>
2025-10-16 14:09:58 +0000

    Еуче

---
+++ task6.py
@@ -0,0 +1,16 @@
+age = input("возраст пользователя: " )
+
+gender = input("пол пользователя: ")
+
+name = input('имя пользователя: ')
+
+town = input('город пользователя: ')
+
+classi = input('класс обучения: ')
+inf = []
+inf.append(age)
+inf.append(gender)
+inf.append(name)
+inf.append(town)
+inf.append(classi)
+print(inf)
